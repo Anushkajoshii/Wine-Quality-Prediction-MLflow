616c1017b7cea19cab97a067aabec1220150cdec
Anushka Joshi <anushkajoshi020503@gmail.com>
2024-09-19 00:28:48 +0530

    data ingestion added

---
+++ src/mlProject/constants/__init__.py
@@ -0,0 +1,5 @@
+from pathlib import Path
+
+CONFIG_FILE_PATH = Path("/Users/anushkajoshi/Desktop/Wine-Quality-Prediction-MLflow/config/config.yaml")
+PARAMS_FILE_PATH = Path("params.yaml")
+SCHEMA_FILE_PATH = Path("schema.yaml")
